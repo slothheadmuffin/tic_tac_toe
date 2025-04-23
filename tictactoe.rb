b661eea695102e26b61e9a6bc363135e066d875e
elgfe <170134446+slothheadmuffin@users.noreply.github.com>
2025-04-23 10:57:20 -0700

    added empty methods and started board

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,17 +1,54 @@
 class Player
-  def initialize
-    
+  def initialize(player,marker)
+    @player=player
+    @marker=marker
   end
 end
 
 class Board
   def initialize
-    
+    #grid
+    @grid=(1..9).to_a
   end
+
+  def show_board
+    puts @grid
+  end
+
+  def valid_move
+  end
+
+  def full_board
+  end
+
+  def wins
+  end
+
 end
 
 class Game
   def initialize
-    
+    @board=Board.new
   end
+
+  def obtain_players
+  end
+
+  def start_game
+    @board.show_board
+  end
+
+  def change_turn
+  end
+
+  def end_game
+  end
+
+  def end_stats
+  end
+
 end
+
+game=Game.new
+game.start_game
+#Methods idk where
